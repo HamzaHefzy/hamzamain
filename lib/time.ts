62e87b5acd7c9d8d1891e8c3fb05087355b78a31
Hamza Zayed <hz321@duke.edu>
2026-09-19 16:32:29 -0400

    Add organization-local time helper

--- lib/time.ts
+++ lib/time.ts
@@ -9,3 +9,12 @@ export function localDateString(date: Date, timeZone: string) {
   const values = Object.fromEntries(parts.map((part) => [part.type, part.value]));
   return values.year + "-" + values.month + "-" + values.day;
 }
+
+export function localTimeString(date: Date, timeZone: string) {
+  return new Intl.DateTimeFormat("en-GB", {
+    timeZone,
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  }).format(date);
+}

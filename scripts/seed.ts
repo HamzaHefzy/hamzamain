73491952e70b638ee1ee3962b2bb32e27a56e553
Hamza Zayed <hz321@duke.edu>
2026-09-19 16:37:24 -0400

    Make database CLI scripts CommonJS-safe

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import postgres from "postgres";
 
+async function main() {
 const url = process.env.DATABASE_URL;
 if (!url) throw new Error("DATABASE_URL is required.");
 
@@ -175,3 +176,10 @@ await sql.begin(async (tx) => {
 
 await sql.end();
 console.log("Seed complete. Sign in as", email);
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});

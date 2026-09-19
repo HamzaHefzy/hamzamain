d17c76a84ee4f1d9963276c3eb52b72b25205808
Hamza Zayed <hz321@duke.edu>
2026-09-19 16:37:27 -0400

    Make database CLI scripts CommonJS-safe

--- scripts/create-org.ts
+++ scripts/create-org.ts
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import postgres from "postgres";
 
+async function main() {
 const url = process.env.DATABASE_URL;
 if (!url) throw new Error("DATABASE_URL is required.");
 
@@ -71,3 +72,10 @@ const result = await sql.begin(async (tx) => {
 await sql.end();
 console.log("Organization created:", result.slug);
 console.log("Owner:", required.adminEmail);
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
